7b7eb2125058f2b7c6d6af5f730bc00ad02324d0
Charles Denzel Sy <61255802+charlessy11@users.noreply.github.com>
2021-02-21 09:13:54 +0800

    Update util.h

--- util.h
+++ util.h
@@ -44,7 +44,7 @@ ssize_t lineread(int fd, char *buf, size_t sz);
 * @param str_ptr point of tokenization
 * @param delim deliminator
 *
-* @return amount of bytes
+* @return token
 */
 char *next_token(char **str_ptr, const char *delim);
 
@@ -54,7 +54,6 @@ char *next_token(char **str_ptr, const char *delim);
 * @param buf stores information
 * @param frac amount of usage
 *
-* @return amount of bytes
 */
 void draw_percbar(char *buf, double frac);
 
@@ -62,9 +61,8 @@ void draw_percbar(char *buf, double frac);
 * Draws percentage bar
 *
 * @param name_buf stores names
-* @param uid the uid
+* @param uid the user identification
 *
-* @return amount of bytes
 */
 void uid_to_uname(char *name_buf, uid_t uid);
 
